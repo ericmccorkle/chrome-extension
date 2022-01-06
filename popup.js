f87fc26a8276a3b3da9e7261f7dfae4b76c77465
Justinlkirk <justinleekirk@gmail.com>
2022-01-06 13:05:32 -0600

    Fully progromatic with new button functionality

--- popup.js
+++ popup.js
@@ -1,19 +1,38 @@
-const button = document.getElementById("translatePage");
-
+const buttons = document.querySelectorAll("button");
+const body = document.querySelector('body');
+let pageLanguage = 'en';
+buttons.forEach((butn, ind) => {
+  if (ind === 0) {
+    butn.addEventListener('click', async () => {
+      const newButton = document.createElement('button');
+      newButton.innerText = window.prompt('Please enter the language you would like to add: ');
+      newButton.id = window.prompt('Please enter 2 letter language code: ');
+      while (newButton.id.length !== 2) {
+        newButton.id = window.prompt('Code must be 2 letters: ');
+      }
+      addEvent(newButton);
+      body.appendChild(newButton);
+    })
+  }
+  else {
+    addEvent(butn);
+  }
+  function addEvent(button) {
+    button.addEventListener("click", async (diff) => {
+      let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+      const translateLanguage = button.id,
+        languages = pageLanguage + translateLanguage;
+    
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: translate, // execute function to transalte 
+        args: [languages]
+      });
+    });
+  }
+})
 
 // When the button is clicked, inject translate into current page
-let pageLanguage = 'en',
-  translateLanguage = 'de',
-  languages = pageLanguage + translateLanguage;
-button.addEventListener("click", async (diff) => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: translate, // execute function to transalte 
-    args: [languages]
-  });
-});
 
 // The body of this function will be execuetd as a content script inside the current page
 function translate(lang) {
