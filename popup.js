e9eedc0ea00d1bdbf04eb47f55631ac595739f72
Justinlkirk <justinleekirk@gmail.com>
2022-01-06 12:22:55 -0600

    Fully functional with 1 button

--- popup.js
+++ popup.js
@@ -1,61 +1,55 @@
-// Initialize button with users's prefered color
-// Initialize button with users's prefered color
-let translatePage = document.getElementById("translatePage"); // Eric note = alter this line
+const button = document.getElementById("translatePage");
 
-// Eric note: alter this file for our own button
-chrome.storage.sync.get("color", ({ color }) => {
-  translatePage.style.backgroundColor = color;
-});
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-// Eric note: This is in reference to the 'green' button. In our case, it will be a button that reads 'Translate'
-translatePage.addEventListener("click", async (diff) => {
+// When the button is clicked, inject translate into current page
+let pageLanguage = 'en',
+  translateLanguage = 'de',
+  languages = pageLanguage + translateLanguage;
+button.addEventListener("click", async (diff) => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: translate, // execute function to transalte 
+    args: [languages]
   });
 });
 
-// The body of this function will be execuetd as a content script inside the
-// current page
-// Eric note: put our functionality here
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
-
-function translate() {
+// The body of this function will be execuetd as a content script inside the current page
+function translate(lang) {
+  const pageLang = lang[0] + lang[1],
+    translateLang = lang[2] + lang[3];
   const pTags = document.querySelectorAll('p');
-  let difficulty = Number(window.prompt('Please enter a difficulty 1 being the hardest with larger numbers being easier: '));
-  while (Math.floor(difficulty) !== difficulty || difficulty < 0 || Number.isNaN(difficulty)) {
-    difficulty = Number(window.prompt('Please enter a whole integer greater than 0: '));
-  }
 
   pTags.forEach((tag) => {
-    pickingOutWords(tag);
+    wrappingWords(tag);
   });
 
-  function pickingOutWords(currTag) {
+  function wrappingWords(currTag) {
     const wordsArr = currTag.innerText.split(' ')
-    const numOfWordsToChange = Math.floor(wordsArr.length / difficulty);
-    //const indexes = generateIndexes(numOfWordsToChange, wordsArr.length);
+    //const numOfWordsToChange = Math.floor(wordsArr.length / difficulty); // Depricated functionality
+    //const indexes = generateIndexes(numOfWordsToChange, wordsArr.length); // Depricated functionality
 
     currTag.innerText = '';
     wordsArr.forEach((word, ind) => {
-      if (Math.random() <= (1/difficulty)) {
-        const newWord = 'TRANSLATE';
-        const HTML = ` <strong> ${newWord} </strong> `
-        currTag.innerHTML += HTML;
-      } else {
-        currTag.innerText += ` ${word}`;
-      }
+      if (true) {
+        const aTag = document.createElement('a'),
+          beginningOfLink = `https://translate.google.com/?sl=${pageLang}&tl=${translateLang}&text=`,
+          endOfLink = '&op=translate';
+        
+        aTag.href = beginningOfLink + word + endOfLink;
+        aTag.innerText = ` ${word}`;
+        currTag.appendChild(aTag);
+      } 
+      // else {                                             // Depricated span tag functionality (pointless with our change in function)
+      //   const spanTag = document.createElement('span');
+      //   spanTag.innerText = ` ${word}`;
+      //   currTag.appendChild(spanTag);
+      // }
 
     })
   }
-
+                                                              // Old less effective way of choosing which words to change
   // function generateIndexes(numOfInd, max) {
   //   let count = 0;
   //   const output = [];
